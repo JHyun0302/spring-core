31ae88b34b57fd38ed34edee95426c7061af2c3a
jaehyun <wogustkfkd11@gmail.com>
2022-12-24 21:37:00 +0900

    스프링 추가 방법

--- src/main/java/hello/core/OrderApp.java
+++ src/main/java/hello/core/OrderApp.java
@@ -7,23 +7,29 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.order.Order;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class OrderApp {
     public static void main(String[] args) {
         AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService();
-        OrderService orderService = appConfig.orderService();
+//        MemberService memberService = appConfig.memberService();
+//        OrderService orderService = appConfig.orderService();
         /**
          * appConfig에서 구현체 생성해서 MemberServiceImpl에 주입해서 인터페이스에 연결
          */
 //        MemberService memberService = new MemberServiceImpl(memberRepository);
 //        OrderService orderService =  new OrderServiceImpl(new MemoryMemberRepository(), discountPolicy);
 
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+        OrderService orderService = applicationContext.getBean("orderService", OrderService.class);
+
         Long memberId = 1L;
         Member member = new Member(memberId, "memberA", Grade.VIP);
         memberService.join(member);
 
-        Order order = orderService.createOrder(memberId, "itemA", 10000);
+        Order order = orderService.createOrder(memberId, "itemA", 20000);
         System.out.println("order = " + order); //Order 객체의 toString()으로 출력
         System.out.println("order.calculatePrice() = " + order.calculatePrice());
     }
