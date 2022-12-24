f7bba620b00ca2663e1101d6e187fdb3496e00e2
jaehyun <wogustkfkd11@gmail.com>
2022-12-24 21:37:07 +0900

    스프링 추가 방법

--- src/main/java/hello/core/MemberApp.java
+++ src/main/java/hello/core/MemberApp.java
@@ -4,12 +4,24 @@ import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
-        MemberService memberService = appConfig.memberService(); // -> memberServiceImpl(new MemoryMemberRepository)
+        /*AppConfig appConfig = new AppConfig();
+        MemberService memberService = appConfig.memberService();*/
+        /**
+         * memberServiceImpl(new MemoryMemberRepository)
+         * appConfig.memberService();에서 구현체 생성해서 memberService에 주입
+          */
 //      MemberService memberService = new MemberServiceImpl(memberRepository); //인터페이스 & 구현체 모두 의존
+
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        //ApplicationContext = Spring Container(객체 관리)
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+        //.getBean("메서드 이름", (반환) 타입)
+
         Member member = new Member(1L, "memberA", Grade.VIP);
         memberService.join(member);
 
