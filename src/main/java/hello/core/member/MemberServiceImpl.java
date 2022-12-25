8fc663e2c00cf47e37b84ee38840946ab5808eb5
jaehyun <wogustkfkd11@gmail.com>
2022-12-25 22:44:16 +0900

    자동 빈 등록: @Component + @Autowired

--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -1,13 +1,18 @@
 package hello.core.member;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class MemberServiceImpl implements MemberService{
 
     private final MemberRepository memberRepository; //= new MemoryMemberRepository(); // 인터페이스와 구현체 모두 의존 자르기
     //추상화에만 의존 = DIP 지킴
+
+    @Autowired //자동 의존관계 주입: AppConfig없이 인터페이스와 구현체 연결 == ac.getBean(MemberRepository.class)
     public MemberServiceImpl(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     }
-
 
     /**
      * MemberRepository memberRepository는 인터페이스(역할)에 의존
@@ -23,5 +28,11 @@ public class MemberServiceImpl implements MemberService{
     @Override
     public Member findMember(Long memberId) {
         return memberRepository.findById(memberId);
+    }
+
+
+    //테스트 용도
+    public MemberRepository getMemberRepository() {
+        return memberRepository;
     }
 }
