df610e3db3c8662a4676f4a570391b95e14554e0
jaehyun <wogustkfkd11@gmail.com>
2022-12-25 22:41:20 +0900

    ComponentScan

---
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -0,0 +1,23 @@
+package hello.core;
+
+import hello.core.member.MemberRepository;
+import hello.core.member.MemoryMemberRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+
+import static org.springframework.context.annotation.ComponentScan.Filter;
+
+@Configuration
+@ComponentScan(  //basePackages 없을 때 default: @ComponentScan이 붙은 설정 정보 클래스의 패키지가 시작 위치
+        basePackages = "hello.core.member", //basePackages: 컴포넌트 스캔 탐색 시작위치 지정
+        basePackageClasses = AutoAppConfig.class, //basePackageClasses: 지정한 클래스의 패키지를 탐색 시작 위치로 지정
+        excludeFilters = @Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
+)
+public class  AutoAppConfig {
+    @Bean(name = "memoryMemberRepository")
+    MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+}
