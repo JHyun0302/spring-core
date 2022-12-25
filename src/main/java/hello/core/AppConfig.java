0b48d9e25a04beb015f1008c7dba178c24ac021b
jaehyun <wogustkfkd11@gmail.com>
2022-12-25 22:39:56 +0900

    스프링 등록 후 싱글톤 call 횟수

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -13,25 +13,51 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration //Spring기반으로 변경
 public class AppConfig { //Config: 설정 정보
+
+    /**
+     * @Bean memberService -> new MemoryMemberRepository()
+     * @Bean orderService -> new MemoryMemberRepository()
+     * 싱글톤 깨지나? A. 안 깨짐
+     */
+    /**
+     * 예상
+     * call AppConfig.memberService
+     * call AppConfig.memberRepository
+     * call AppConfig.memberRepository
+     * call AppConfig.orderService
+     * call AppConfig.memberRepository
+     *
+     * 실제
+     * call AppConfig.memberService
+     * call AppConfig.memberRepository
+     * call AppConfig.orderService
+     */
+
+
     @Bean //Spring Container에 등록
     public MemberService memberService(){ //인터페이스: 역할
-        return new MemberServiceImpl(memberRepository()); //Extract Method: Command + Option + M
+        System.out.println("call AppConfig.memberService");
+        return new MemberServiceImpl(memberRepository());
+//        return new MemberServiceImpl(new MemoryMemberRepository()); //@Configuataion빼고 돌리면  memberService 호출할 때마다 MemoryMemberRepository 생성함
     }
 
     /**
      * 리팩토링: new를 하는 곳 따로 만들어서 중복 없앰
+     * Extract Method: Command + Option + M
      */
     @Bean //(name = "changeName") //스프링 빈에 등록되는 이름 바꾸기
-    public static MemoryMemberRepository memberRepository() { //구현체: 구현
+    public MemoryMemberRepository memberRepository() { //구현체: 구현
+        System.out.println("call AppConfig.memberRepository");
         return new MemoryMemberRepository();
     }
     @Bean
     public OrderService orderService(){ //인터페이스: 역할
+        System.out.println("call AppConfig.orderService");
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
     @Bean
     public DiscountPolicy discountPolicy(){ //구현체: 구현
-        return new FixDiscountPolicy();
-//        return new RateDiscountPolicy();
+//        return new FixDiscountPolicy();
+        return new RateDiscountPolicy();
     }
 }
