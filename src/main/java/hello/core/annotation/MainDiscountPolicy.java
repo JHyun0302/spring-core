4fdcfd0201bd6ff0f46950c30409087f6a3725bc
jaehyun <wogustkfkd11@gmail.com>
2022-12-27 00:00:47 +0900

    나만의 @

---
+++ src/main/java/hello/core/annotation/MainDiscountPolicy.java
@@ -0,0 +1,17 @@
+package hello.core.annotation;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+
+import java.lang.annotation.*;
+
+@Target({ElementType.FIELD, ElementType.METHOD, ElementType.PARAMETER, ElementType.TYPE, ElementType.ANNOTATION_TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Inherited
+@Documented
+@Qualifier("mainDiscountPolicy")
+public @interface MainDiscountPolicy {
+}
+
+/**
+ * MainDiscountPolicy - annotation사용시 @ 모두 사용
+ */
