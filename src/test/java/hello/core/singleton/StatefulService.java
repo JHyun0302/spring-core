d447037b83efba834037d75608ba17ebad2ab633
jaehyun <wogustkfkd11@gmail.com>
2023-02-04 14:32:28 +0900

    싱글톤 패턴시 price 변수를 공유 변수로 취급하지 않을려면 int로 반환시켜서 각자 다른 값을 넘김

--- src/test/java/hello/core/singleton/StatefulService.java
+++ src/test/java/hello/core/singleton/StatefulService.java
@@ -3,16 +3,16 @@ package hello.core.singleton;
 public class StatefulService { //test 만들기: command + shift + T
 //    private int price; //상태를 유지하는 필드
 
+    /**
+     * 사용자A가 1만원 주문하고 사용자B가 2만원 주문했을 때
+     * price가 싱글톤에 의해 공유 변수라서 2만원으로 바뀜
+     * 사용자A가 주문금액 확인할 때 2만원으로 확인함!!
+     */
 //    public void order(String name, int price)
-    public int order(String name, int price){
+    public int order(String name, int price){ //서로 다른 변수로 return 시키면 해결
         System.out.println("name = " + name + " price = " + price);
-//        this.price = price; //여기가 문제!
+//        this.price = price; //여기가 문제!(공유 변수)
         return price;
-        /**
-         * 사용자A가 1만원 주문하고 사용자B가 2만원 주문했을 때
-         * price가 싱글톤에 의해 공유 변수라서 2만원으로 바뀜
-         * 사용자A가 주문금액 확인할 때 2만원으로 확인함!!
-         */
     }
 
 /*    public int getPrice(){
