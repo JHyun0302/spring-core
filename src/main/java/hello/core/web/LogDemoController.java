a902b272914a7b4cba8b186d4bd3663b597efe3f
jaehyun <wogustkfkd11@gmail.com>
2023-02-05 23:25:37 +0900

    Web Scope는 DL  필요

--- src/main/java/hello/core/web/LogDemoController.java
+++ src/main/java/hello/core/web/LogDemoController.java
@@ -12,16 +12,21 @@ import javax.servlet.http.HttpServletRequest;
 @Controller
 @RequiredArgsConstructor
 public class LogDemoController {
+    /**
+     * Web Scope: 이 빈은 실제 고객의 요청이 와야 생성 가능!
+     * 해결법1. 즉 DL이 필요! -> ObjectProvider
+     * 해결법2. 프록시 생성
+     * 핵심: 진짜 객체 조회를 꼭 필요한 시점까지 지연처리 한다는 점!!
+     */
+//    private final ObjectProvider<MyLogger> myLoggerProvider;
     private final LogDemoService logDemoService;
-
-//    private final ObjectProvider<MyLogger> myLoggerProvider; //Provider: myLogger를 찾을 수 있는 DL이 주입 됨
     private final MyLogger myLogger;
 
     @RequestMapping("log-demo")
     @ResponseBody
     public  String logDemo(HttpServletRequest request) throws InterruptedException {  //HttpServletRequest: 자바에서 제공하는 표준 Servlet 규약(고객 요청 정보 받음)
         String requestURL = request.getRequestURL().toString();//고객이 어떤 url로 요청했는지 파악 가능
-//        MyLogger myLogger = myLoggerProvider.getObject();
+//        MyLogger myLogger = myLoggerProvider.getObject(); //ObjectProvider
 
         System.out.println("myLogger = " + myLogger.getClass()); // 가짜 프록시 주입
         myLogger.setRequestURL(requestURL); //기능 수행할 때 진짜를 찾아서 동작
